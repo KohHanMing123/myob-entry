3a6f5a5aabe628fdd20b9478b37eb5bfdb2d11cf
KohHanMing123 <kohhanming123@gmail.com>
2026-01-20 21:52:47 +0800

    feat: 3rd line addition

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ providers = ['Friends Provident International Limited',
              'Utmost Worldwide Ltd',
              'Singapore Life Ltd',
              'Monument Intl Life Ass Co Ltd']
-beginner = [49100, 49150, 47000, 47100, 40500, 41000]
+beginner = [49100, 49150, 47000, 47100, 40500, 41000, 41016, 44016, 47116, 47516]
 
 # loading data
 wb = load_workbook('data.xlsx', data_only=True)
@@ -54,7 +54,7 @@ ws = wb.active
 
 # fetching and packing data
 data = dict()
-collumns = list('ABCDEFGHI')
+collumns = list('ABCDEFGHIJK')
 for i in range(len(ws['A'][1:])):
     if ws['A'][i + 1].internal_value is None:
         continue
@@ -64,10 +64,10 @@ for i in range(len(ws['A'][1:])):
     data[i] = tuple(d)
 
 
-# value: 0=provider 1=invoicedate 2=desc 3=adviser 4=tax 5=v1 6=c1 7=v2 8=c2
+# value: 0=provider 1=invoicedate 2=desc 3=adviser 4=tax 5=v1 6=c1 7=v2 8=c2 9=v3 10=c3
 # data validation and confirmation
 unknowns = {'Provider': {}, 'Invoice Date': {}, 'Description': {}, 'Adviser': {}, 'Tax': {}, 'Value 1': {},
-            'Code 1': {}, 'Value 2': {}, 'Code 2': {}}
+            'Code 1': {}, 'Value 2': {}, 'Code 2': {}, 'Value 3': {}, 'Code 3': {}}
 for rowno, values in data.items():
     rowno += 2
     if values[0] not in providers:
@@ -92,13 +92,16 @@ for rowno, values in data.items():
     if values[7] is not None:
         if values[8] is None or len(str(values[8])) != 5:
             unknowns['Code 2'].setdefault(values[8], []).append(rowno)
+    if values[9] is not None:
+        if values[10] is None or len(str(values[10])) != 5:
+            unknowns['Code 3'].setdefault(values[10], []).append(rowno)
 fatalerrors = False
 errorno = 0
 uerrorno = 0
 warnno = 0
 uwarnno = 0
 for Type, errors in unknowns.items():
-    if Type in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2']:
+    if Type in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2', 'Code 3']:
         uerrorno += len(errors)
         for error in errors.values():
             errorno += len(error)
@@ -106,7 +109,7 @@ for Type, errors in unknowns.items():
         uwarnno += len(errors)
         for warning in errors.values():
             warnno += len(warning)
-input(f'''Total number of fatal errors: {errorno}
+input(f'''PYTHON PROGRAM v2: Total number of fatal errors: {errorno}
 Number of unique fatal errors: {uerrorno}
 Total number of warnings: {warnno}
 Number of unique warnings: {uwarnno}
@@ -115,13 +118,14 @@ press enter to continue''')
 for Type, errors in unknowns.items():
     if len(errors) == 0:
         continue
-    if Type in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2']:
+    if Type in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2', 'Code 3']:
         print(
             f'fatal error of [{Type}] in the following rows, please amend or remove invalid entry and rerun program '
             f'to continue:')
         fatalerrors = True
         for error, rownos in errors.items():
-            print(f'\t{f'"{error}"' if error is not None else 'MISSING VALUE'}:')
+            error_display = f'"{error}"' if error is not None else 'MISSING VALUE'
+            print(f'\t{error_display}:')
             for num in rownos:
                 print(f'\t\t\t- {num}')
 
@@ -133,11 +137,12 @@ print('\n--------------------------------------------------------\n')
 for Type, errors in unknowns.items():
     if len(errors) == 0:
         continue
-    if Type not in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2']:
+    if Type not in ['Invoice Date', 'Adviser', 'Value 1', 'Code 1', 'Code 2', 'Code 3']:
         print(f'warnings for [{Type}] in the following rows, please amend before rerunning program or ignore by '
               f'pressing enter for every warning: ')
         for error, rownos in errors.items():
-            print(f'\t{f'"{error}"' if error is not None else 'MISSING VALUE'}:', end="")
+            error_display = f'"{error}"' if error is not None else 'MISSING VALUE'
+            print(f'\t{error_display}:', end="")
             for num in rownos:
                 print(f'\n\t\t\t- {num}', end="")
             input()
@@ -148,9 +153,11 @@ countdown(5)
 for value in data.values():
     invoicedate = value[1]
     date = datetime(int(invoicedate[4:]), int(invoicedate[2:4]), int(invoicedate[:2])).strftime('%d/%m/%Y')
-    bs = f'{value[0]}\n\t\t\t\t\t\t{invoicedate}\t{date}\t{invoicedate}\t{value[2]}\t{code(value[6], value[3])}\t\t{value[5]}\t\t\t{value[4]}'
+    bs = f'{value[0]}\n\t\t\t\t\t\t\t\t{invoicedate}\t{date}\t{invoicedate}\t{value[2]}\t{code(value[6], value[3])}\t\t{value[5]}\t\t\t{value[4]}'
     if value[7] is not None and value[8] is not None:
         bs += f'\t\t{value[2]}\t{code(value[8], value[3])}\t\t{value[7]}\t\t\t{value[4]}'
+    if value[9] is not None and value[10] is not None:
+        bs += f'\t\t{value[2]}\t{code(value[10], value[3])}\t\t{value[9]}\t\t\t{value[4]}'
     pyag.click(161, 83)
     pyag.typewrite(bs)
     if value[4] == 'ZR':
@@ -161,8 +168,14 @@ for value in data.values():
     time.sleep(0.5)
     pyag.click(180, 860)
     pyag.typewrite(value[2])
-    input('finished')
-    pyag.click(700, 962)
     time.sleep(0.4)
-    pyag.click(880, 610)
+    # Record the entry
+    pyag.click(880, 958)
+    time.sleep(0.4)
+    pyag.click(880, 608)
+    #clicks the prompt that comes up when you click the record button
+    # pyag.click(700, 962) 
+    # the dropdown below the tax column
+    # input("clicked record")
     time.sleep(0.5)
+input('finished')
